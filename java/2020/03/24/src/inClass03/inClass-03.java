98bb464bb0e950cc92736ad79f2ccf160f994683
FishMagic <fish_magic@ftmc.me>
2020-03-24 20:09:54 +0800

    JavaHomework-20200324#3-FixInClass03

--- java/2020/03/24/src/inClass03/inClass-03.java
+++ java/2020/03/24/src/inClass03/inClass-03.java
@@ -23,8 +23,8 @@ class Point{
         Point p1 = new Point();
         Point p2 = new Point(Math.random()*5,Math.random()*7);
         Point p1d,p2d;
-        p1d=p1.movePoint((int) Math.random()*9,(int) Math.random()*18);
-        p2d=p2.movePoint((int) Math.random()*17,(int) Math.random()*22);
+        p1d=p1.movePoint((int) (Math.random()*26),(int) (Math.random()*18));
+        p2d=p2.movePoint((int) (Math.random()*17),(int) (Math.random()*22));
         System.out.printf("P1移动前的坐标为(%f,%f)，移动后为(%f,%f)\n",p1.x,p1.y,p1d.x,p1d.y);
         System.out.printf("P2移动前的坐标为(%f,%f)，移动后为(%f,%f)\n",p2.x,p2.y,p2d.x,p2d.y);
     }
