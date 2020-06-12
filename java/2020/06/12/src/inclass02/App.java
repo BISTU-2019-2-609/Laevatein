e07e3e2e80b3071932a945327e550e671f3dda92
Laevatein Scarlet <fish_magic@ftmc.me>
2020-06-12 22:00:23 +0800

    JavaHomework-20200612#2

--- java/2020/06/12/src/inclass02/App.java
+++ java/2020/06/12/src/inclass02/App.java
@@ -1,16 +1,13 @@
 package inclass02;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
